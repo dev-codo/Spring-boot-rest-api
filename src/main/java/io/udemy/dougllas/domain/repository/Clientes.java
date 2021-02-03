f98e45071ab61f426833f1535332da6a911eacf6
liaocmq <liao.wei@chronometriq.ca>
2021-02-03 17:18:33 -0500

    Convert other CRUD methods to Jpa

--- src/main/java/io/udemy/dougllas/domain/repository/Clientes.java
+++ src/main/java/io/udemy/dougllas/domain/repository/Clientes.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -34,47 +35,64 @@ public class Clientes {
         return cliente;
     }
 
+    @Transactional
     public Cliente atualizar(Cliente cliente) {
-        jdbcTemplate.update(UPDATE,
-                cliente.getNome(),
-                cliente.getId()
-        );
+//        jdbcTemplate.update(UPDATE, cliente.getNome(), cliente.getId()); // com JdbcTemplate
+        entityManager.merge(cliente); // com Jpa
         return cliente;
     }
 
+    @Transactional
     public void deletar(Cliente cliente) {
-        deletar(cliente.getId());
+//        deletar(cliente.getId()); // com JdbcTemplate
+        if (!entityManager.contains(cliente)) {
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente); // com Jpa
     }
 
+    @Transactional
     public void deletar(Integer id) {
-        jdbcTemplate.update(DELETE, id);
+//        jdbcTemplate.update(DELETE, id); // com JdbcTemplate
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        deletar(cliente);
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> buscarPorNome(String nome) {
-        return jdbcTemplate.query(
-                SELECT_ALL.concat(" where nome like ?"),
-//                new Object[]{"%" + nome + "%"}, // deprecated
-                obterClienteRowMapper(),
-                "%" + nome + "%");
+//        return jdbcTemplate.query(        // com JdbcTemplate
+//                SELECT_ALL.concat(" where nome like ?"),
+//                obterClienteRowMapper(),
+//                "%" + nome + "%");
+
+        String jpql = "select c from Cliente c where c.nome like :nome";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, obterClienteRowMapper());
+//        return jdbcTemplate.query(SELECT_ALL, obterClienteRowMapper());
+        return entityManager
+                .createQuery("from Cliente", Cliente.class)
+                .getResultList(); // com Jpa
     }
 
-    private RowMapper<Cliente> obterClienteRowMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
-//                Integer id = rs.getInt("id");
-//                String nome = rs.getString("nome");
-//                return new Cliente(id, nome); // OU...
-
-                Cliente cliente = new Cliente();
-                cliente.setId(rs.getInt("id"));
-                cliente.setNome(rs.getString("nome"));
-                return cliente;
-            }
-        };
-    }
+//    com JdbcTemplate
+//    private RowMapper<Cliente> obterClienteRowMapper() {
+//        return new RowMapper<Cliente>() {
+//            @Override
+//            public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
+////                Integer id = rs.getInt("id");
+////                String nome = rs.getString("nome");
+////                return new Cliente(id, nome); // OU...
+//
+//                Cliente cliente = new Cliente();
+//                cliente.setId(rs.getInt("id"));
+//                cliente.setNome(rs.getString("nome"));
+//                return cliente;
+//            }
+//        };
+//    }
 }
