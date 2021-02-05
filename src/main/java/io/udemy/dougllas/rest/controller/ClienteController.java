22d9ac1f0964522b90e1696bd4670329f75aba54
liaocmq <liao.wei@chronometriq.ca>
2021-02-05 16:38:06 -0500

    41: Refac to best practices and cleaner - ClienteController

--- src/main/java/io/udemy/dougllas/rest/controller/ClienteController.java
+++ src/main/java/io/udemy/dougllas/rest/controller/ClienteController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
@@ -25,73 +23,65 @@ public class ClienteController {
 //        this.clientes = clientes;
 //    }
 
-    @GetMapping("/all")
-    public ResponseEntity getAllClientes(Cliente cliente) {
-        List<Cliente> clienteAll = clientes.findAll();
+    @GetMapping
+    public List<Cliente> getAllClientes(Cliente cliente) {
+//        List<Cliente> clienteAll = clientes.findAll();
+//
+//        if (clienteAll.isEmpty()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        return new ResponseEntity<>(clienteAll, HttpStatus.OK);
 
-        if (clienteAll.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return new ResponseEntity<>(clienteAll, HttpStatus.OK);
+        return clientes.findAll();
     }
 
     @GetMapping("{id}")
-//    @ResponseBody //not really necessary
-    public ResponseEntity getClienteById(@PathVariable Integer id) {
-        Optional<Cliente> cliente = clientes.findById(id);
-
-        if (cliente.isPresent()) {
-            return ResponseEntity.ok(cliente.get());
-        }
-
-//        return ResponseEntity.notFound().build(); OR...
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public Cliente getClienteById(@PathVariable Integer id) {
+        return clientes
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado")); // http 404
     }
 
     @PostMapping
-//    @ResponseBody //not really necessary
-    public ResponseEntity salvar(@RequestBody Cliente cliente) {
-        Cliente clienteSalvo = clientes.save(cliente);
-        return ResponseEntity.ok(clienteSalvo);
+    @ResponseStatus(HttpStatus.CREATED) // http 201
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return clientes.save(cliente);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity deletar(@PathVariable Integer id) {
-
-        Optional<Cliente> cliente = clientes.findById(id);
-
-        if (cliente.isPresent()) {
-            clientes.delete(cliente.get());
-//            return ResponseEntity.noContent().build(); ...OR
-            return new ResponseEntity(HttpStatus.NO_CONTENT); // http 204
-        }
-
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NO_CONTENT) // http 204
+    public void deletar(@PathVariable Integer id) {
+        clientes.findById(id)
+                .map(cl -> {
+                    clientes.delete(cl);
+                    return cl;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity updater(@PathVariable Integer id, @RequestBody Cliente cliente) {
-        return clientes
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updater(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        clientes
                 .findById(id)
                 .map(clienteExistente -> {
                     cliente.setId(clienteExistente.getId());
                     clientes.save(cliente);
-                    return new ResponseEntity(HttpStatus.OK);
-                }).orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+                    return clienteExistente;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
     }
 
-    /* Search/filter a cliente*/
+    /* Search/filter a cliente. Work as findAll as well, if no query params is entered in Postman */
     @GetMapping("/search")
-    public ResponseEntity procurar(Cliente filtro) {
+    public List<Cliente> procurar(Cliente filtro) {
         ExampleMatcher matcher = ExampleMatcher
                                     .matching()
                                     .withIgnoreCase()
                                     .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
 
         Example example = Example.of(filtro, matcher);
-        List<Cliente> lista = clientes.findAll(example);
-        return ResponseEntity.ok(lista);
+        return clientes.findAll(example);
     }
 
 }
